693bffccddf1d3f60d5d8bf0bd3f52e0bd6097f5
Joz Fama <jm_rg@comunidad.unam.mx>
2026-08-19 19:25:20 -0600

    refactor(form.habit): optimize form habit component

--- src/app/components/form.habit/form.habit.component.ts
+++ src/app/components/form.habit/form.habit.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { DaysOfWeek } from 'src/app/enums/days.enum';
-import { Habit } from 'src/app/models/habit';
-import { AuthService } from 'src/app/services/auth.service';
-import { ModalService } from 'src/app/services/modal.service';
-import { add, edit, find, resetHabit } from 'src/app/store/habit/habits.action';
-import { CalendarProgressComponent } from "../calendar.progress/calendar.progress.component";
-import { IonContent, IonCard, IonCardHeader, IonRow, IonCol, IonIcon, IonCardContent, IonLabel, IonItem, IonDatetimeButton, IonDatetime, IonModal, IonCardTitle, IonCheckbox, IonAccordionGroup, IonAccordion, IonButton, IonInput, IonToggle } from "@ionic/angular/standalone";
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { IonAccordion, IonAccordionGroup, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCheckbox, IonCol, IonContent, IonDatetime, IonDatetimeButton, IonIcon, IonInput, IonItem, IonLabel, IonModal, IonRow, IonText, IonToggle } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { closeSharp } from 'ionicons/icons';
+import { finalize } from 'rxjs';
+import { BaseForm } from 'src/app/classes/base.form';
+import { APP, GENERAL, VIEWS } from 'src/app/constants/constants';
+import { DaysOfWeek } from 'src/app/enums/days.enum';
+import { ModalActions } from 'src/app/enums/modal.actions.enum';
+import { Habit } from 'src/app/models/dtos/habit.model';
+import { ModalService } from 'src/app/services/modal.service';
+import { HabitState } from 'src/app/states/habit.state';
+import { dateToDatetimeStr } from 'src/app/utils/helpers';
+import { CalendarProgressComponent } from "../calendar.progress/calendar.progress.component";
 
 @Component({
   selector: 'app-form.habit',
   standalone: true,
-  imports: [
+  imports: [IonText,
     IonButton, IonAccordion, IonAccordionGroup, IonCheckbox, IonCardTitle, IonModal, IonDatetimeButton, IonItem, IonLabel, IonCardContent, IonIcon, IonCol, IonRow, IonCardHeader, IonCard, IonContent, IonInput, IonToggle, IonDatetime,
     RouterLink,
     FormsModule,
@@ -24,123 +28,104 @@ import { closeSharp } from 'ionicons/icons';
   templateUrl: './form.habit.component.html',
   styleUrls: ['./form.habit.component.scss'],
 })
-export class FormHabitComponent  implements OnInit {
+export class FormHabitComponent extends BaseForm implements OnInit {
 
-  loading ! : boolean;
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly habitState = inject(HabitState);
+  private readonly modalService = inject(ModalService);
 
-  habit ! : Habit;
+  habitForm : Partial<Habit> = { days: [] };
   daysOfWeek = DaysOfWeek;
 
-  errors : any = {}
-  errorHour : boolean = false;
-  
-  view ! : string;
+  view = signal<string>('');
+  hourAllowed = false;
+  errorHour = false;
 
-  hourAllowed : boolean;
-  
-  constructor(
-    private store : Store<{habits : any, loading : any}>,
-    private route : ActivatedRoute,
-    private authService : AuthService,
-    private modalService : ModalService
-  ) {
-    addIcons({
-      closeSharp
-    });
-    this.habit = new Habit();
-    this.hourAllowed = false;
-
-    this.store.select('habits').subscribe(state => {
-      this.habit = {... state.habit};
-      this.errors = state.errors;
-    });
-
+  constructor() {
+    super();
+    addIcons({closeSharp});
   }
 
   ngOnInit(): void {
-    this.store.dispatch(resetHabit());
     this.route.paramMap.subscribe(params => {
-      const id : number = + (params.get('id') || '0');
-      if(id > 0){
-        this.store.dispatch(find({id}));
-        
-        if(this.habit.hour){
-          this.hourAllowed = true;
+      const id = Number(params.get(APP.ID)) || GENERAL.ZERO;
+      const viewMode = params.get(APP.VIEW) || GENERAL.EMPTY_STRING;
+
+      this.view.set(viewMode);
+
+      if (id > 0) {
+        const existingHabit = this.habitState.habits().find(h => h.id === id);
+
+        if (existingHabit) {
+          this.habitForm = { ...existingHabit };
+          this.hourAllowed = !!this.habitForm.hour;
+        } else {
+          this.router.navigate([VIEWS.LIST_HABITS]);
         }
-        
+      } else {
+        this.habitForm = { days: [] };
+        this.hourAllowed = false;
       }
-      this.view = params.get('view') || '';
-
-      if(this.view !== 'view' && this.view !== 'edit'){
-        this.store.dispatch(resetHabit());
-      }
-
-    })
+    });
   }
 
-  onHourAllowed(event : Event ) : void {
-    const onHourCheck = event.target as HTMLInputElement;
-    if(onHourCheck.checked){
-      this.hourAllowed = true;
-    }else{
-      this.hourAllowed = false;
-      this.habit.hour = '';
+  onHourAllowed(event : any ) : void {
+    this.hourAllowed = event.detail.checked;
+    if (!this.hourAllowed) {
+      this.habitForm.hour = undefined;
+    } else if (!this.habitForm.hour) {
+      this.habitForm.hour = dateToDatetimeStr(new Date());
     }
   }
 
-  onSubmit(habitForm : NgForm) : void {
+  onSubmit() : void {
     this.errorHour = false;
-    this.errors = {}
+    this.clearErrors();
 
-    if(this.hourAllowed){
-      if(this.habit.hour == undefined || this.habit.hour == ''){
+    if (this.hourAllowed) {
+      if (!this.habitForm.hour) {
         this.errorHour = true;
         return;
       }
 
-      const date = new Date(this.habit.hour);
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
-      this.habit.hour = `${hours}:${minutes}`;
-    }
-
-    if(this.habit.id != null){
-      this.store.dispatch(edit({habitUpdate: this.habit}));
-    }else{
-      const payload = this.authService.getPayload();
-      if(payload && payload.sub){
-        this.habit.userId = Number(payload.sub);
-        this.store.dispatch(add({habitNew: this.habit}));
+      if (this.habitForm.hour.includes(GENERAL.SEPARATE_DATETIME)) {
+        this.habitForm.hour = dateToDatetimeStr(new Date(this.habitForm.hour));
       }
-    } 
-    this.modalService.showModal("loading");
-  }
-
-  toggleAllDays(){
-    this.toggleDay(DaysOfWeek.MONDAY);
-    this.toggleDay(DaysOfWeek.TUESDAY);
-    this.toggleDay(DaysOfWeek.WEDNESDAY);
-    this.toggleDay(DaysOfWeek.THURSDAY);
-    this.toggleDay(DaysOfWeek.FRIDAY);
-    this.toggleDay(DaysOfWeek.SATURDAY);
-    this.toggleDay(DaysOfWeek.SUNDAY);
-  }
-
-  toggleDay(day : DaysOfWeek ){
-    if(!this.habit.days){
-      this.habit.days = [];
     }
 
-    const updateDays = [... this.habit.days];
-    const index = updateDays.indexOf(day);
+    this.modalService.showModal(ModalActions.LOADING);
 
-    if(index === -1){
-      updateDays.push(day);
-    }else{
-      updateDays.splice(index, 1);
-    }
-
-    this.habit.days = updateDays; 
+    this.habitState.saveHabit(this.habitForm).pipe(
+      finalize(() => this.modalService.dismissLoading())
+    ).subscribe({
+      next: () => {
+        this.router.navigate([VIEWS.LIST_HABITS]);
+      },
+      error: (err : HttpErrorResponse) => {
+        this.handleFormError(err);
+      }
+    });
   }
 
+  toggleAllDays() {
+    const allDays = Object.values(DaysOfWeek);
+
+    if (this.habitForm.days?.length === allDays.length) {
+      this.habitForm.days = [];
+    } else {
+      this.habitForm.days = [...allDays];
+    }
+  }
+
+  toggleDay(day : DaysOfWeek){
+    const currentDays = this.habitForm.days || [];
+    const index = currentDays.indexOf(day);
+
+    if (index === -1) {
+      this.habitForm.days = [...currentDays, day];
+    } else {
+      this.habitForm.days = currentDays.filter(d => d !== day);
+    }
+  }
 }
