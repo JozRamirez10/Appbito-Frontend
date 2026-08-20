6557b5142e776522d0d52fc12b6bcbccfde72cca
Joz Fama <jm_rg@comunidad.unam.mx>
2026-08-19 19:11:51 -0600

    fix(helpers): fix and optimize helpers

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,46 +1,44 @@
+import { HttpErrorResponse, HttpStatusCode } from "@angular/common/http";
+import { catchError, Observable, throwError } from "rxjs";
+import { APP, ERRORS, GENERAL } from "../constants/constants";
 import { DaysOfWeek } from "../enums/days.enum";
-import { HabitProgress } from "../models/habit";
+import { ModalActions } from "../enums/modal.actions.enum";
+import { Months } from "../enums/months.enum";
+import { HabitProgress } from "../models/dtos/habit.progress.model";
+import { ModalService } from "../services/modal.service";
 
 /**********************
  * Date
 **********************/
 
-export const weekDays : string[] = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-
-export const monthNames : string[] = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
-];
-
-export function dayWihtoutTime(date : Date) : Date{
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-}
-
 export function getDayOfWeek() : DaysOfWeek{
-    const today : DaysOfWeek = ( () => {
-      const dayMap = [
-        DaysOfWeek.SUNDAY,
-        DaysOfWeek.MONDAY,
-        DaysOfWeek.TUESDAY,
-        DaysOfWeek.WEDNESDAY,
-        DaysOfWeek.THURSDAY,
-        DaysOfWeek.FRIDAY,
-        DaysOfWeek.SATURDAY
-      ];
-      return dayMap[new Date().getDay()];
-    })()
-    return today;
+    const dayMap = [
+      DaysOfWeek.SUNDAY, DaysOfWeek.MONDAY, DaysOfWeek.TUESDAY,
+      DaysOfWeek.WEDNESDAY, DaysOfWeek.THURSDAY, DaysOfWeek.FRIDAY, DaysOfWeek.SATURDAY
+    ]
+    return dayMap[new Date().getDay()];
 }
 
-export const daysMap:{ [kay in DaysOfWeek] : number } = {
-  [DaysOfWeek.SUNDAY]: 0,
-  [DaysOfWeek.MONDAY]: 1,
-  [DaysOfWeek.TUESDAY]: 2,
-  [DaysOfWeek.WEDNESDAY]: 3,
-  [DaysOfWeek.THURSDAY]: 4,
-  [DaysOfWeek.FRIDAY]: 5,
-  [DaysOfWeek.SATURDAY]: 6,
-};
+export function dateToDatetimeStr(date : Date) : string {
+  const hour = date.getHours().toString().padStart(APP.HOUR_CHARS, GENERAL.ZERO_STRING);
+  const minutes = date.getMinutes().toString().padStart(APP.MINUTES_CHARS,
+    GENERAL.ZERO_STRING);
+  return[hour, minutes].join(GENERAL.COLON);
+}
+
+export function formatDateToString(date : Date) : string {
+
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = (d.getMonth() + 1).toString().padStart(2, GENERAL.ZERO_STRING);
+  const day = d.getDate().toString().padStart(2, GENERAL.ZERO_STRING);
+
+  return [year, month, day].join(GENERAL.HYPEN);
+}
+
+export function todayToString() : string {
+  return formatDateToString(new Date());
+}
 
 export function isToday(day: Date): boolean {
   const today = new Date();
@@ -50,10 +48,12 @@ export function isToday(day: Date): boolean {
   return false;
 }
 
-export function dayExists(day : Date, days : Date[]) : boolean{
-  return days.some(d => {
-    return compareDay(d, day);
-  });
+export function decodeDateString(dateString : string)
+  : { year : number, month : number, day : number } {
+
+  const [year, month, day] = dateString.split(GENERAL.HYPEN).map(Number);
+
+  return { year, month: month - 1, day };
 }
 
 export function compareDay(day1 : Date, day2: Date) : boolean {
@@ -62,27 +62,12 @@ export function compareDay(day1 : Date, day2: Date) : boolean {
     day1.getFullYear() === day2.getFullYear();
 }
 
-export function getWeeklyRanges(day : Date){
+export function getTwoMonthlyRangesForDate(date : Date){
+  const prevMonthDate = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+  const nextMonthDate = new Date(date.getFullYear(), date.getMonth() + 1, 1);
 
-  const startOfWeek = new Date(day);
-  startOfWeek.setDate(day.getDate() - day.getDay() + 1); // Monday
-
-  const endOfWeek = new Date(startOfWeek);
-  endOfWeek.setDate(startOfWeek.getDate() + 6); // Sunday
-
-  return {
-    start: startOfWeek.toISOString().split('T')[0],
-    end: endOfWeek.toISOString().split('T')[0]
-  }
-}
-
-export function getTwoMonthlyRangesForToday(){
-  const today = dayWihtoutTime(new Date())
-  const lastDayMonth = dayWihtoutTime(new Date(today.getFullYear(), today.getMonth() - 1, today.getDate()));
-  const nextDayMonth = dayWihtoutTime(new Date(today.getFullYear(), today.getMonth() + 1, today.getDate()));
-
-  const lastMonth = getMonthlyRanges(lastDayMonth);
-  const nextMonth = getMonthlyRanges(nextDayMonth);
+  const lastMonth = getMonthlyRanges(prevMonthDate);
+  const nextMonth = getMonthlyRanges(nextMonthDate);
 
   return {
     start: lastMonth.start,
@@ -91,78 +76,86 @@ export function getTwoMonthlyRangesForToday(){
 }
 
 export function getMonthlyRanges(day : Date) {
-  // Current Month
   const startOfCurrentMonth = new Date(day.getFullYear(), day.getMonth(), 1);
   const endOfCurrentMonth = new Date(day.getFullYear(), day.getMonth() + 1, 0);
 
   return {
-    start: startOfCurrentMonth.toISOString().split('T')[0],
-    end: endOfCurrentMonth.toISOString().split('T')[0]
+    start: startOfCurrentMonth.toISOString().split(GENERAL.SEPARATE_DATETIME)[0],
+    end: endOfCurrentMonth.toISOString().split(GENERAL.SEPARATE_DATETIME)[0]
   }
+}
+
+export function getMonthsFromRange(startDate : string, endDate: string )
+  : { year : number, month : number }[] {
+
+  const start = decodeDateString(startDate);
+  const end = decodeDateString(endDate);
+
+  const period = [];
+  let currentYear = start.year;
+  let currentMonth = start.month;
+
+  while (currentYear < end.year || (currentYear === end.year && currentMonth <= end.month)) {
+    period.push({ year: currentYear, month: currentMonth });
+    currentMonth++;
+
+    if (currentMonth > Months.DEC) {
+      currentMonth = Months.JAN;
+      currentYear++;
+    }
+  }
+
+  return period;
 }
 
 /**********************
  * Progress
 **********************/
 
-export function getDone(day : Date, habitProgress : HabitProgress[]) : number {
-  const progress = habitProgress.find(progress => compareDay(day, progress.date) && progress.timesPerformed >= 1);
-    
-    if(progress){
-      return progress.timesPerformed;
-    }
+export function getMonthlyProgressKey(year : number, month : number, habitId : number) : string {
+  return [year, month, habitId].join(GENERAL.HYPEN);
+}
 
-    return 0;
+export function getDone(day : Date, habitProgress : HabitProgress[]) : number {
+
+  if (!day || !habitProgress) return 0;
+
+  const dayStr = formatDateToString(day);
+  const progress = habitProgress.find(p => p.date === dayStr && p.timesPerformed >= 1);
+
+  return progress ? progress.timesPerformed : 0;
 }
 
 export function hasNote(day : Date, habitProgress : HabitProgress[]) : boolean {
-  return habitProgress.some(progress => compareDay(day, progress.date) && progress.note && progress.note?.length > 0);
+
+  if(!day || !habitProgress) return false;
+
+  const dayStr = formatDateToString(day);
+  return habitProgress.some(p => p.date === dayStr && p.note && p.note.length > 0);
 }
 
-export function compareHabitProgress(progress1 : HabitProgress[], progress2 : HabitProgress[]) : boolean {
-  if(progress1.length !== progress2.length)
-    return false;
-  return progress1.every(prog1 => progress2.some(prog2 => compareDay(prog1.date, prog2.date)));
+/**********************
+ * Errors
+**********************/
+
+export function hasNotErrorInterceptors(err : HttpErrorResponse) :  boolean {
+    return !ERRORS.ERROR_STATUS_INTERCEPTOR.includes(err.status)
 }
 
-export function getStreak(progress: Date[], days: DaysOfWeek[]): number {
-  if (progress.length === 0) return 0;
+export function notifyError(err : HttpErrorResponse, action : ModalActions,
+  modalService : ModalService, ignoreBadRequest = false) : void {
 
-  const allowedDaysArray = days.map(day => daysMap[day]);
-  const allowedDays = new Set(allowedDaysArray);
+  if (!hasNotErrorInterceptors(err)) return;
 
-  const filteredProgress = progress
-    .filter(date => allowedDays.has(date.getDay()))
-    .sort((a, b) => a.getTime() - b.getTime());
+  if (ignoreBadRequest && err.status === HttpStatusCode.BadRequest) return;
 
-  if (filteredProgress.length === 0) return 0;
+  modalService.showModal(action);
+}
 
-  let maxStreak = 0;
-  let currentStreak = 0;
-  let countDay = 0;
-
-  filteredProgress.reduce((prev, day, index) => {    
-    
-    if (currentStreak === 0) {
-      countDay = allowedDaysArray.findIndex(d => d === day.getDay());
-    }
-
-    const currentDay = day.getDay();
-
-    const diffDays = (day.getTime() - prev.getTime()) / (1000 * 60 * 60 * 24)
-
-    if (currentDay === allowedDaysArray[countDay] && diffDays < 8 ) {
-      currentStreak++;
-    } else {
-      maxStreak = Math.max(maxStreak, currentStreak);
-      currentStreak = 1;
-    }
-
-    countDay = (countDay + 1) % allowedDaysArray.length;
-
-    return day;
-  }, filteredProgress[0]);
-
-  maxStreak = Math.max(maxStreak, currentStreak);
-  return maxStreak;
+export function catchAndNotify<T>(action : ModalActions, modalService : ModalService,
+  ignoreBadRequest = false) {
+  return catchError<T, Observable<never>>((err : HttpErrorResponse) => {
+      notifyError(err, action, modalService, ignoreBadRequest);
+      return throwError(() => err);
+  });
 }
